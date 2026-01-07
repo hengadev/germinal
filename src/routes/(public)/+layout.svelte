55179cd09c54bfe7d28c0ff92d736935004ff9db
GaryHY <henry.gary@hotmail.com>
2026-01-07 22:02:00 +0100

    feat(footer): add Instagram link and update styling

--- src/routes/(public)/+layout.svelte
+++ src/routes/(public)/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import Navigation from "$lib/components/Navigation.svelte";
-    import type { LayoutData } from './$types';
+    import type { LayoutData } from "./$types";
 
-    let { data, children }: { data: LayoutData, children: any } = $props();
+    let { data, children }: { data: LayoutData; children: any } = $props();
 </script>
 
 <div class="min-h-screen flex flex-col">
@@ -12,11 +12,22 @@
         {@render children()}
     </main>
 
-    <footer class="bg-dark-900 text-white py-6 mt-12">
-        <div class="container mx-auto px-4 text-center">
+    <footer class="bg-dark-900 text-white/80 py-6 mt-12">
+        <div class="container mx-auto px-4 flex items-center justify-between">
             <p>
                 &copy; {new Date().getFullYear()} Germinal. All rights reserved.
             </p>
+            <div class="flex gap-8">
+                <a
+                    class="hover:text-white"
+                    href="https://www.instagram.com/Germinal.studio/"
+                    >Instagram</a
+                >
+                <!-- <a -->
+                <!--     class="hover:text-white" -->
+                <!--     href="https://www.instagram.com/Germinal.studio/">Terms</a -->
+                <!-- > -->
+            </div>
         </div>
     </footer>
 </div>
